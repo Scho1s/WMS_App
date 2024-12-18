1aca015d3ab4847680535ce710ef82edd79b9b35
Scho1s <jurkin.valerij@gmail.com>
2024-12-18 14:49:34 +0000

    Refactored Receiving bit, completely removed Putaway and Picking

--- wmsapp/src/wmsapp/app.py
+++ wmsapp/src/wmsapp/app.py
@@ -26,131 +26,36 @@ class WMSApp(toga.App):
         self.main_window.content = self.main_box
         self.main_window.show()
 
-    def create_windows(self):
-        self.create_receiving_window()
-        self.create_picking_window()
-        self.create_putaway_window()
-
-    def create_receiving_window(self):
-        self.r_sku_label = toga.Label("SKU:",
-                                      style=Pack(padding=self.LABEL_PADDING,
-                                                 alignment=RIGHT))
-
-        self.r_sku_entry = toga.TextInput()
-
-        self.no_sku_found_label = toga.Label("No such SKU found.",
-                                             style=Pack(color=RED,
-                                                        padding=self.DEF_PADDING,
-                                                        ))
-
-        self.sku_label_box = toga.Box(style=Pack(padding=self.LABEL_PADDING),
-                                      children=[self.r_sku_label,])
-
-        self.sku_entry_box = toga.Box(style=Pack(padding=self.ENTRY_PADDING,
-                                                 direction=COLUMN,
-                                                 flex=1),
-                                      children=[self.r_sku_entry,])
-
-        self.r_inner_sku_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
-                                                   direction=ROW,
-                                                   flex=1),
-                                        children=[self.sku_label_box,
-                                                  self.sku_entry_box,
-                                                  ])
-
-        self.no_sku_label_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
-                                                    flex=1,
-                                                    alignment=CENTER
-                                                    ),
-                                         children=[self.no_sku_found_label,
-                                                   ])
-
-        self.r_outer_sku_box = toga.Box(style=Pack(direction=COLUMN,
-                                                   alignment=CENTER,
-                                                   ),
-                                        children=[self.r_inner_sku_box,
-                                                  ])
-
-        enter_box = toga.Box(style=Pack(direction=COLUMN,
-                                        flex=1),
-                             children=[self.enter_button,
-                                       ]
-                             )
-
-        back_box = toga.Box(style=Pack(direction=COLUMN,
-                                       flex=1),
-                            children=[self.back_button,
-                                      ]
-                            )
-
-        lower_menu_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
-                                             direction=ROW),
-                                  children=[enter_box,
-                                            back_box,
-                                            ]
-                                  )
-
-        self.receiving_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
-                                                 direction=COLUMN),
-                                      children=[self.r_outer_sku_box,
-                                                self.spacer,
-                                                lower_menu_box,
-                                                ])
-
-    def create_putaway_window(self):
-        self.putaway_box = toga.Box(style=Pack(direction=COLUMN))
-        self.pa_header = toga.Label("Putaway")
-        self.pa_label_box = toga.Box(style=Pack(direction=COLUMN))
-        self.pa_label_box.add(self.pa_header)
-        self.putaway_box.add(self.pa_label_box)
-
-    def create_picking_window(self):
-        self.picking_box = toga.Box(style=Pack(direction=COLUMN))
-        self.p_header = toga.Label("Picking")
-        self.p_label_box = toga.Box(style=Pack(direction=COLUMN))
-        self.p_label_box.add(self.p_header)
-        self.picking_box.add(self.p_label_box)
-
     def create_helpers(self):
-        self.back_button = toga.Button(
-            "Back",
-            on_press=self.back,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       alignment=BOTTOM)
-        )
-
         self.spacer = toga.Box(
             id="spacer",
-            style=Pack(flex=1),
+            style=Pack(flex=1,
+                       direction=COLUMN),
         )
 
-        self.enter_button = toga.Button(
-            "Enter",
-            on_press=self.r_enter,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       alignment=BOTTOM)
-        )
+    def create_windows(self):
+        self.__create_receiving_main_window()
+        self.create_picking_window()
+        self.create_putaway_window()
 
     def add_main_menu(self):
         self.receiving_button = toga.Button(
             "Receiving",
-            on_press=self.receiving,
+            on_press=self.enter_receiving_main,
             style=Pack(padding=self.DEF_PADDING,
                        font_size=self.MENU_FONT_SIZE),
         )
 
         self.putaway_button = toga.Button(
             "Put away",
-            on_press=self.putaway,
+            on_press=self.enter_putaway_main,
             style=Pack(padding=self.DEF_PADDING,
                        font_size=self.MENU_FONT_SIZE)
         )
 
         self.picking_button = toga.Button(
             "Picking",
-            on_press=self.picking,
+            on_press=self.enter_picking_main,
             style=Pack(padding=self.DEF_PADDING,
                        font_size=self.MENU_FONT_SIZE),
         )
@@ -159,27 +64,77 @@ class WMSApp(toga.App):
         self.main_box.add(self.putaway_button)
         self.main_box.add(self.picking_button)
 
-    def receiving(self, widget):
+    # Receiving
+
+    def __create_receiving_main_window(self):
+        self.r_sku_label = toga.Label("SKU:",
+                                      style=Pack(padding=self.LABEL_PADDING,
+                                                 alignment=RIGHT))
+
+        self.r_sku_entry = toga.TextInput(style=Pack(direction=COLUMN,
+                                                     flex=1))
+
+        self.no_sku_found_label = toga.Label("No such SKU found.",
+                                             style=Pack(color=RED,
+                                                        padding=self.DEF_PADDING,
+                                                        flex=1,
+                                                        direction=COLUMN,
+                                                        ))
+
+        self.r_enter_button = toga.Button(
+            "Enter",
+            on_press=self.enter_receiving_detail,
+            style=Pack(padding=self.DEF_PADDING,
+                       font_size=self.MENU_FONT_SIZE,
+                       flex=1)
+        )
+
+        self.r_back_button = toga.Button(
+            "Back",
+            on_press=self.back_to_main,
+            style=Pack(padding=self.DEF_PADDING,
+                       font_size=self.MENU_FONT_SIZE,
+                       flex=1)
+        )
+
+        self.sku_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
+                                           flex=1),
+                                children=[self.r_sku_label,
+                                          self.r_sku_entry,
+                                          ])
+
+        self.r_outer_sku_box = toga.Box(style=Pack(direction=COLUMN,
+                                                   alignment=CENTER,
+                                                   ),
+                                        children=[self.sku_box,
+                                                  self.no_sku_found_label,
+                                                  ])
+
+        self.lower_menu_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
+                                                  direction=ROW),
+                                       children=[self.r_enter_button,
+                                                 self.r_back_button,
+                                                 ]
+                                       )
+
+        self.receiving_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
+                                                 direction=COLUMN),
+                                      children=[self.r_outer_sku_box,
+                                                self.lower_menu_box,
+                                                ])
+
+    def enter_receiving_main(self, widget):
+        self.__clear_receiving_window()
+        self.main_window.content = self.receiving_box
+
+    def __clear_receiving_window(self):
         self.r_sku_entry.value = ""
         self.__remove_no_sku_label()
 
-        self.main_window.content = self.receiving_box
-
-    def putaway(self, widget):
-        self.putaway_box.add(self.spacer)
-        self.putaway_box.add(self.back_button)
-        self.main_window.content = self.putaway_box
-
-    def picking(self, widget):
-        self.picking_box.add(self.spacer)
-        self.picking_box.add(self.back_button)
-        self.main_window.content = self.picking_box
-
-    def r_enter(self, widget):
+    def enter_receiving_detail(self, widget):
         sku = self.r_sku_entry.value
         if sku == "111":
-            self.r_sku_entry.value = ""
-            self.__remove_no_sku_label()
+            self.__clear_receiving_window()
             self.receive_product(sku)
         else:
             self.r_outer_sku_box.insert(1, self.no_sku_label_box)
@@ -187,14 +142,27 @@ class WMSApp(toga.App):
     def receive_product(self, sku):
         print(f"Received {sku}")
 
-    def __draw_receive_product_box(self):
-        pass
-
-    def back(self, widget):
-        self.main_window.content = self.main_box
-
     def __remove_no_sku_label(self):
         self.r_outer_sku_box.remove(self.no_sku_label_box)
+
+    # Putaway
+
+    def create_putaway_window(self):
+        pass
+
+    def enter_putaway_main(self, widget):
+        pass
+
+    # Picking
+
+    def create_picking_window(self):
+        pass
+
+    def enter_picking_main(self, widget):
+        pass
+
+    def back_to_main(self, widget):
+        self.main_window.content = self.main_box
 
 
 def main():
