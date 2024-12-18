da012eb80ddf0aed8b061369086c819af7d0ed07
Scho1s <jurkin.valerij@gmail.com>
2024-12-18 21:43:49 +0000

    Created default styles and fonts

--- wmsapp/src/wmsapp/app.py
+++ wmsapp/src/wmsapp/app.py
@@ -4,106 +4,102 @@ WMS application for android scanners to work together with Dynamics GP
 
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW, BOTTOM, CENTER, RIGHT
+from toga.style.pack import COLUMN, CENTER, RIGHT
 from toga.constants import RED
 
 
 class WMSApp(toga.App):
     MENU_FONT_SIZE = 20
+    ERROR_FONT_SIZE = 15
     DEF_PADDING = 5
-    TOP_MENU_PADDING = (30, DEF_PADDING, DEF_PADDING, DEF_PADDING)
+    DEF_BUTTON_FONT = "Roboto_Thin"
+    DEF_LABEL_FONT = "Roboto_Black"
+    DEF_ERROR_FONT = "Roboto_Medium"
     LABEL_PADDING = (DEF_PADDING, 1, DEF_PADDING, DEF_PADDING)
     ENTRY_PADDING = (DEF_PADDING, DEF_PADDING, DEF_PADDING, 1)
+    DEF_ENTRY_STYLE = Pack(direction=COLUMN, flex=1)
+    DEF_LABEL_STYLE = Pack(padding=LABEL_PADDING, font_family=DEF_LABEL_FONT, alignment=RIGHT, width=75)
+    DEF_MAIN_MENU_STYLE = Pack(padding=DEF_PADDING, font_size=MENU_FONT_SIZE, font_family=DEF_BUTTON_FONT)
+    DEF_LOW_MENU_STYLE = Pack(padding=DEF_PADDING, font_size=MENU_FONT_SIZE, font_family=DEF_BUTTON_FONT, flex=1)
+    DEF_ERROR_STYLE = Pack(padding=DEF_PADDING, font_size=ERROR_FONT_SIZE, font_family=DEF_ERROR_FONT, alignment=RIGHT, color=RED)
+    DEF_SPACER_STYLE = Pack(direction=COLUMN, flex=1)
 
     def startup(self):
+        self.__create_helpers()
+        self.__create_windows()
+        self.__add_main_menu()
+
         self.main_box = toga.Box(style=Pack(direction=COLUMN,
-                                            padding=self.TOP_MENU_PADDING))
-        self.create_helpers()
-        self.create_windows()
-        self.add_main_menu()
+                                            padding=(10, self.DEF_PADDING, self.DEF_PADDING, self.DEF_PADDING)),
+                                 children=[self.receiving_button,
+                                           self.putaway_button,
+                                           self.picking_button,
+                                           ])
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = self.main_box
         self.main_window.show()
 
-    def create_helpers(self):
-        self.spacer = toga.Box(
-            id="spacer",
-            style=Pack(flex=1,
-                       direction=COLUMN),
-        )
+    def __create_helpers(self):
+        self.spacer = toga.Box(style=self.DEF_SPACER_STYLE)
 
-    def create_windows(self):
+        toga.Font.register("Roboto_Thin", "resources/Roboto-Thin.ttf")
+        toga.Font.register("Roboto_Black", "resources/Roboto-BlackItalic.ttf")
+        toga.Font.register("Roboto_Medium", "resources/Roboto-Medium.ttf")
+
+    def __create_windows(self):
         self.__create_receiving_main_window()
         self.__create_receiving_detail_window()
         self.create_picking_window()
         self.create_putaway_window()
 
-    def add_main_menu(self):
+    def __add_main_menu(self):
         self.receiving_button = toga.Button(
             "Receiving",
             on_press=self.enter_receiving_main,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE),
+            style=self.DEF_MAIN_MENU_STYLE,
         )
 
         self.putaway_button = toga.Button(
             "Put away",
             on_press=self.enter_putaway_main,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE)
+            style=self.DEF_MAIN_MENU_STYLE,
         )
 
         self.picking_button = toga.Button(
             "Picking",
             on_press=self.enter_picking_main,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE),
+            style=self.DEF_MAIN_MENU_STYLE,
         )
-
-        self.main_box.add(self.receiving_button)
-        self.main_box.add(self.putaway_button)
-        self.main_box.add(self.picking_button)
 
     # Receiving
 
     def __create_receiving_main_window(self):
         self.r_sku_label = toga.Label("SKU:",
-                                      style=Pack(padding=self.LABEL_PADDING,
-                                                 alignment=RIGHT))
+                                      style=self.DEF_LABEL_STYLE)
 
         self.r_sku_entry = toga.TextInput(style=Pack(direction=COLUMN,
                                                      flex=1))
 
         self.no_sku_found_label = toga.Label("",
-                                             style=Pack(color=RED,
-                                                        padding=self.DEF_PADDING,
-                                                        alignment=RIGHT,
-                                                        ))
+                                             style=self.DEF_ERROR_STYLE)
 
-        self.r_spacer = toga.Box(
-            id="r_spacer",
-            style=Pack(flex=1,
-                       direction=COLUMN),
-        )
+        self.r_spacer = toga.Box(style=self.DEF_SPACER_STYLE)
 
         self.r_enter_button = toga.Button(
             "Enter",
             on_press=self.enter_receiving_detail,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       flex=1)
+            style=self.DEF_LOW_MENU_STYLE,
         )
 
         self.r_back_button = toga.Button(
             "Back",
             on_press=self.back_to_main,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       flex=1)
+            style=self.DEF_LOW_MENU_STYLE,
         )
 
         self.sku_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
+                                           alignment=RIGHT,
                                            flex=1),
                                 children=[self.r_sku_label,
                                           self.r_sku_entry,
@@ -128,7 +124,7 @@ class WMSApp(toga.App):
                                                ])
 
         self.lower_menu_box = toga.Box(style=Pack(padding=self.DEF_PADDING,
-                                                  direction=ROW),
+                                                  ),
                                        children=[self.r_enter_button,
                                                  self.r_back_button,
                                                  ])
@@ -148,28 +144,29 @@ class WMSApp(toga.App):
                                             style=Pack(padding=self.LABEL_PADDING))
 
         self.r_quantity_label = toga.Label("Quantity: ",
-                                           style=Pack(padding=self.LABEL_PADDING, alignment=RIGHT, width=100))
+                                           style=self.DEF_LABEL_STYLE)
 
-        self.r_quantity_entry = toga.TextInput(style=Pack(direction=COLUMN,
-                                                          flex=1))
+        self.r_quantity_entry = toga.NumberInput(style=self.DEF_ENTRY_STYLE)
 
         self.r_weight_label = toga.Label("Weight: ",
-                                         style=Pack(padding=self.LABEL_PADDING, alignment=RIGHT, width=100))
+                                         style=self.DEF_LABEL_STYLE)
 
-        self.r_weight_entry = toga.TextInput(style=Pack(direction=COLUMN,
-                                                        flex=1))
+        self.r_weight_entry = toga.NumberInput(style=self.DEF_ENTRY_STYLE, step=0.001)
 
         self.r_best_before_label = toga.Label("Best Before: ",
-                                              style=Pack(padding=self.LABEL_PADDING, alignment=RIGHT, width=100))
+                                              style=self.DEF_LABEL_STYLE)
 
-        self.r_best_before_entry = toga.TextInput(style=Pack(direction=COLUMN,
-                                                             flex=1))
+        self.r_best_before_entry = toga.DateInput(style=self.DEF_ENTRY_STYLE)
 
         self.r_batch_code_label = toga.Label("Batch Code: ",
-                                             style=Pack(padding=self.LABEL_PADDING, alignment=RIGHT, width=100))
+                                             style=self.DEF_LABEL_STYLE)
 
-        self.r_batch_code_entry = toga.TextInput(style=Pack(direction=COLUMN,
-                                                            flex=1))
+        self.r_batch_code_entry = toga.TextInput(style=self.DEF_ENTRY_STYLE)
+
+        self.r_tag_label = toga.Label("Tag: ",
+                                      style=self.DEF_LABEL_STYLE)
+
+        self.r_tag_entry = toga.TextInput(style=self.DEF_ENTRY_STYLE)
 
         self.r_spacer2 = toga.Box(
             id="r_spacer2",
@@ -180,17 +177,13 @@ class WMSApp(toga.App):
         self.r_save_button = toga.Button(
             "Save",
             on_press=self.r_save_details,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       flex=1)
+            style=self.DEF_LOW_MENU_STYLE,
         )
 
         self.r_back_button2 = toga.Button(
             "Back",
             on_press=self.back_to_receiving,
-            style=Pack(padding=self.DEF_PADDING,
-                       font_size=self.MENU_FONT_SIZE,
-                       flex=1)
+            style=self.DEF_LOW_MENU_STYLE,
         )
 
         self.r_item_details_box = toga.Box(style=Pack(padding=self.DEF_PADDING),
@@ -208,10 +201,13 @@ class WMSApp(toga.App):
         self.r_batch_code_box = toga.Box(style=Pack(padding=self.DEF_PADDING),
                                          children=[self.r_batch_code_label, self.r_batch_code_entry])
 
+        self.r_tag_box = toga.Box(style=Pack(padding=self.DEF_PADDING),
+                                  children=[self.r_tag_label, self.r_tag_entry])
+
         self.r_spacer2_box = toga.Box(style=Pack(direction=COLUMN, flex=1),
                                       children=[self.r_spacer2])
 
-        self.r_lower_menu2_box = toga.Box(style=Pack(padding=self.DEF_PADDING, direction=ROW),
+        self.r_lower_menu2_box = toga.Box(style=Pack(padding=self.DEF_PADDING),
                                           children=[self.r_save_button, self.r_back_button2])
 
         self.r_details_box = toga.Box(style=Pack(padding=self.DEF_PADDING, direction=COLUMN),
@@ -220,21 +216,30 @@ class WMSApp(toga.App):
                                                 self.r_weight_box,
                                                 self.r_best_before_box,
                                                 self.r_batch_code_box,
+                                                self.r_tag_box,
                                                 self.r_spacer2_box,
                                                 self.r_lower_menu2_box,
                                                 ])
 
     def enter_receiving_main(self, widget):
-        self.__clear_receiving_window()
+        self.__clear_receiving_main_window()
         self.main_window.content = self.r_main_box
 
-    def __clear_receiving_window(self):
+    def __clear_receiving_main_window(self):
         self.r_sku_entry.value = ""
         self.r_outer_sku_box.remove(self.no_sku_label_box)
 
+    def __clear_receiving_details_window(self):
+        self.r_quantity_entry.value = None
+        self.r_weight_entry.value = None
+        self.r_best_before_entry.value = None
+        self.r_batch_code_entry.value = None
+        self.r_tag_entry.value = None
+
     def enter_receiving_detail(self, widget):
         sku = self.r_sku_entry.value
-        self.__clear_receiving_window()
+        self.__clear_receiving_main_window()
+        self.__clear_receiving_details_window()
         if sku == "111":
             self.main_window.content = self.r_details_box
         else:
@@ -245,7 +250,7 @@ class WMSApp(toga.App):
         print(f"Received {sku}")
 
     def r_save_details(self, widget):
-        pass
+        print(self.r_best_before_entry.value)
 
     def back_to_receiving(self, widget):
         self.enter_receiving_main(widget)
